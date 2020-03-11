9aa8038039f9855bcda8c53a34ee52fdf4a6bf5d
Raphael Memmesheimer <raphael@uni-koblenz.de>
2020-03-11 15:28:58 +0100

    clean up

--- train.py
+++ train.py
@@ -4,50 +4,16 @@ from classification_module import ClassificationLightningModule
 import hydra
 from omegaconf import DictConfig
 
-# import torch
-# from torch.nn import functional as F
-# from torch.nn import Conv2d, Linear, MaxPool2d
-# from torch.utils.data import DataLoader
-# from torchvision.datasets import CIFAR10
-# from torchvision import transforms
-# from torchvision import models
-# import torchvision
-
 import pytorch_lightning as pl
 from pytorch_lightning.logging import TensorBoardLogger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
-# from torch_lr_finder import LRFinder
-# from torchvision import datasets
 
-import argparse
 from argparse import Namespace
 
-# from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, Resize
-# from albumentations.pytorch import ToTensor
-
-# from albumentations import *
-# from albumentations.pytorch import ToTensor
-
-# from warmup_scheduler import GradualWarmupScheduler
-# from onecyclelr import OneCycleLR 
-
-import helpers
 import yaml
 import time
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--config', metavar='FILE', type=str,
-#                               help=' config file ')
-#args = parser.parse_args()
-#print(args.config)
-#config = yaml.load(open(args.config))
-#hparams = Namespace(**config)
-#
-#
-## hparams = #parser.parse_args()
-## 
-#print(hparams)
 @hydra.main(config_path="config/config.yaml")
 def train_app(cfg):
     print(cfg.pretty())
@@ -59,9 +25,7 @@ def train_app(cfg):
     hparams.model_dir = hparams.data_dir+"/"+hparams.model_dir+"/"+hparams.model_name
     
     model = ClassificationLightningModule(hparams=hparams)
-    #model.load_from_checkpoint("/datahdd/raphael/datasets/ntu/ntu_no_border/cross_subject/logs/2020-02-15_11:20/efficientnet/lightning_logs/version_0/checkpoints/_ckpt_epoch_17.ckpt")
-    
-    # most basic trainer, uses good defaults
+
     trainer = pl.Trainer(min_epochs=hparams.min_epochs,
                  max_epochs=hparams.max_epochs,
                  default_save_path=hparams.save_path,
