b29cf9b7b2cf7ccbfb914472d35dae17adee700d
Raphael Memmesheimer <raphael@uni-koblenz.de>
2020-07-04 00:17:25 +0200

    test

--- test.py
+++ test.py
@@ -19,8 +19,9 @@ def test_app(cfg):
     print(cfg.pretty())
     hparams = Namespace(**cfg)
 
-    model = ClassificationLightningModule.load_from_checkpoint(hparams.model_dir)
-    trainer = Trainer()
+    #model = ClassificationLightningModule.load_from_checkpoint(os.environ["DATASET_FOLDER"]+hparams.model_dir+"/_ckpt_epoch_103.ckpt")
+    model = ClassificationLightningModule.load_from_checkpoint(os.environ["DATASET_FOLDER"]+"ntu_reindexing_testreindexing/cross_subject/models_high/efficient/augment_translate/transform_300/_ckpt_epoch_103.ckpt")
+    trainer = Trainer(gpus=-1)
     trainer.test(model)
 
 if __name__ == "__main__":
